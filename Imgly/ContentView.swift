9f77d5104f9965bb4b0e8e9bd9ffebd54d21e45b
haykct <https://github.com/HaykoHayrapetyan>
2023-12-21 14:34:00 +0100

    Added new project.

---
+++ Imgly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Imgly
+//
+//  Created by Hayk Hayrapetyan on 20.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Imgly/ImglyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImglyApp.swift
+//  Imgly
+//
+//  Created by Hayk Hayrapetyan on 20.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ImglyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
