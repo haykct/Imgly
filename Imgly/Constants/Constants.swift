ec27991ae9173445c788335ae7870bbb44ddb78e
haykct <https://github.com/HaykoHayrapetyan>
2023-12-22 11:51:46 +0100

    Added constants to the project. Addded change color theme button.

---
+++ Imgly/Constants/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Imgly
+//
+//  Created by Hayk Hayrapetyan on 22.12.23.
+//
+
+import SwiftUI
+
+enum Fonts {
+    enum Inter {
+        static let regular = "Inter-Regular"
+        static let medium = "Inter-Medium"
+        static let semiBold = "Inter-SemiBold"
+        static let bold = "Inter-Bold"
+    }
+}
+
+enum Colors {
+    static let listBackgroundGrey = Color(.listBackgroundGrey)
+    static let listDividerGrey = Color(.listDividerGrey)
+    static let listIconTurquoise = Color(.listIconTurquoise)
+    static let listTextGrey = Color(.listTextGrey)
+    static let navBarBlue = Color(.navBarBlue)
+}
--- Imgly/Views/ContentView.swift
+++ Imgly/Views/ContentView.swift
@@ -13,51 +13,48 @@ struct ContentView: View {
 
     var body: some View {
         NavigationStack {
-            VStack(spacing: 0) {
-                Image(systemName: "doc.badge.gearshape.fill")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                    .padding()
-                    .background(Color(red: 127 / 255, green: 214 / 255, blue: 191 / 255))
-                    .foregroundStyle(.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .padding()
-                List($users, id: \.self, editActions: .all) { $user in
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text(user)
-                            .padding([.top, .bottom], 12)
-                            .frame(maxHeight: .infinity)
-                            .foregroundStyle(Color(red: 120 / 255, green: 120 / 255, blue: 120 / 255))
-                        Divider()
-                            .frame(height: 1)
-                            .overlay(Color(red: 225 / 255, green: 225 / 255, blue: 225 / 255))
-                    }
-                    .listRowBackground(Color(red: 247 / 255, green: 249 / 255, blue: 250 / 255))
-                    .listRowSeparator(.hidden)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+            List($users, id: \.self, editActions: .all) { $user in
+                VStack(alignment: .leading, spacing: 0) {
+                    Text(user)
+                        .font(.custom(Fonts.Inter.medium, size: 20))
+                        .padding([.top, .bottom], 13)
+                        .frame(maxHeight: .infinity)
+                        .foregroundStyle(Colors.listTextGrey)
+                    Divider()
+                        .frame(height: 1)
+                        .overlay(Colors.listDividerGrey)
                 }
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbarColorScheme(.dark, for: .navigationBar)
-                .toolbarBackground(Color(red: 86 / 255, green: 124 / 255, blue: 150 / 255), for: .navigationBar)
-                .toolbarBackground(.visible, for: .navigationBar)
-                .background(Color(red: 247 / 255, green: 249 / 255, blue: 250 / 255))
-                .listStyle(.plain)
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        Image(systemName: "gearshape.fill")
-                    }
+                .listRowBackground(Colors.listBackgroundGrey)
+                .listRowSeparator(.hidden)
+                .listRowInsets(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarColorScheme(.dark, for: .navigationBar)
+            .toolbarBackground(Colors.navBarBlue, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .listStyle(.plain)
+            .background(Colors.listBackgroundGrey)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
 
-                    ToolbarItem(placement: .principal) {
-                        Text("Title")
-                    }
+                    }, label: {
+                        Image("colorSettings")
+                            .resizable()
+                            .frame(width: 23, height: 23)
+                    })
+                }
 
-                    ToolbarItem(placement: .topBarTrailing) {
-                        EditButton()
-                    }
+                ToolbarItem(placement: .principal) {
+                    Text("Title")
+                        .font(.custom(Fonts.Inter.semiBold, size: 20))
+                }
+
+                ToolbarItem(placement: .topBarTrailing) {
+                    EditButton()
+                        .font(.custom(Fonts.Inter.regular, size: 20))
                 }
             }
-            .background(Color(red: 247 / 255, green: 249 / 255, blue: 250 / 255))
         }
     }
 }
